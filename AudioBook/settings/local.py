968e76ff298fdaf88be1aaecc2badbe89f347001
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-10-11 22:36:05 +0500

    Added tags

--- AudioBook/settings/local.py
+++ AudioBook/settings/local.py
@@ -62,6 +62,7 @@ THIRD_APPS = [
     "corsheaders",
     'django_celery_results',
     'django_celery_beat',
+    'taggit',
 ]
 
 LOCAL_APPS = [
@@ -260,8 +261,11 @@ DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'audiobook-50fe7.appspot.com'
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(FIREBASE_ADMIN_CREDENTIALS)
 
-# Optional: Define media URL for Firebase-hosted files
-# MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/"
+Optional: Define media URL for Firebase-hosted files
+MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/"
+
+# looking up existing tags
+TAGGIT_CASE_INSENSITIVE = True
 
 if __name__ == '__main__':
     ic(BASE_DIR)
