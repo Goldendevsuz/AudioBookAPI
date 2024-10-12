af004344f2e3f0ad2ccedbea2276152f615b4bf9
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-10-12 19:28:35 +0500

    Added tags api crud

--- apps/book/serializers.py
+++ apps/book/serializers.py
@@ -6,11 +6,16 @@ from apps.book.models import Book, BookReview
 
 class BookSerializer(serializers.ModelSerializer):
     author_name = serializers.CharField(source='author.name', read_only=True)
+    tags = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
         # fields = '__all__'
-        fields = ['poster_url', 'cover_url', 'title', 'author_name', 'release_date']
+        fields = ['poster_url', 'cover_url', 'title', 'author_name', 'release_date', 'rate', 'pages_count', 'tags']
+
+    def get_tags(self, obj):
+        return [tag.name for tag in obj.tags.all()]
+
 
 
 class BookReviewSerializer(serializers.ModelSerializer):
